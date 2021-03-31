ef1f1bf3f4e3390f1efb90727e0bc60d936067cb
Sergei <sejego@taltech.ee>
2021-03-31 14:01:16 +0300

    Add grid size feature

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# etch-a-sketch
+# Etch-a-Sketch
 This is a simple Etch-a-sketch web app for The Odin Project made with HTML, CSS and vanilla JavaScript.
+
+## How to view
+
+In order to view the project, press on the following [link](https://sejego.github.io/etch-a-sketch)
--- script.js
+++ script.js
@@ -1,15 +1,62 @@
 const cleanerDiv = document.getElementById("menu");
 const menuButtons = cleanerDiv.children;
-
+const mainBoxSize = 680;
 
 function menuHandler(element)
-
 {
     switch(element.target.textContent) {
         case "Clear":
           clearGrid();
           break;
+        case "Grid size":
+            changeSize();
       } 
+}
+
+function changeSize()
+{
+    var size = prompt("Enter the size from 1 to 64")
+            if (size !== null) {
+                size = parseInt(size);
+                if (size < 1 || size > 64 || Number.isNaN(size)) {
+                    alert("Enter a number from 1-64 range");
+                    changeSize();
+                }else{
+                    setGrid(size);
+                }
+            }else{
+                alert("Enter a number from 1-64 range");
+                changeSize()
+            }
+}
+
+function setGrid(gridSize)
+{
+    mainDiv.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    mainDiv.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+    var sizeX = gridSize*gridSize;
+    sideSize = Math.sqrt((mainBoxSize*mainBoxSize)/sizeX) - 2;
+    regenerateCells(mainDiv, sizeX, sideSize);
+}
+
+function regenerateCells(parent, newCellsNumber, cellSize)
+{
+    let divArr = [];
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+    for(i=0;i<newCellsNumber;i++){
+        divArr[i] = document.createElement('div');
+        divArr[i].classList.add('box');
+    }
+    appendChildren(mainDiv, divArr);
+    sideSizePx = cellSize.toString()+'px';
+    for(i=0;i<mainDiv.children.length;i++)
+    {
+        mainDiv.children[i].style.width = sideSizePx;
+        mainDiv.children[i].style.height = sideSizePx;
+    }
+    addHoverListeners(mainDiv.children);
 }
 
 function clearGrid()
@@ -19,6 +66,7 @@ function clearGrid()
         mainDiv.children[i].style.backgroundColor = 'white';
     }
 }
+
 function addClickListeners(elements)
 {
     for(i=0; i<elements.length; i++)
@@ -33,23 +81,20 @@ function appendChildren(parent, children)
         parent.appendChild(child);
     })
 }
+
 function addHoverListeners(elements)
 {
-    /*
-    elements.forEach(function (element){
-        element.addEventListener('pointerenter', setElementBackground);
-    })
-    */
    for(i=0; i< elements.length; i++)
    {
        elements[i].addEventListener('pointerenter',setElementBackground);
    }
 }
+
 function setElementBackground(element){
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
     element.target.style.backgroundColor = "#" + randomColor;
     //console.log(element.target);
-  }
+}
 
 let mainDiv = document.getElementById("main-div");
 let arrLength = 400;
@@ -62,3 +107,5 @@ for(i=0; i<arrLength; i++)
 appendChildren(mainDiv, divArr);
 addHoverListeners(mainDiv.children);
 addClickListeners(menuButtons);
+/* Set initial Grid*/
+setGrid(20);
